18c76536b1155edd3d826dd83678e52d5af1b2fc
Sergey Ufocoder <xufocoder@gmail.com>
2023-12-16 20:49:30 +0300

    feat: add solution for day-16

---
+++ day-16.ts
@@ -0,0 +1,22 @@
+type FindIndex<T extends any[], Accumulator extends any[] = []> 
+	= T extends [infer Head, ...infer Tail]
+		? Head extends '🎅🏼'
+			? Accumulator['length']
+			: FindIndex<Tail, [...Accumulator, false]>
+		: false
+
+type MapRows<T extends any[], Accumulator extends any[] = []>
+	= T extends [infer Head, ...infer Tail]
+		? Head extends any[]
+			? MapRows<Tail, [...Accumulator, FindIndex<Head>]>
+			: MapRows<Tail, [...Accumulator, false]>
+		: Accumulator;
+
+type FindRow<T extends any[], Accumulator extends any[] = []>
+	= T extends [infer Head, ...infer Tail]
+		? Head extends number
+			? [Accumulator['length'], Head]
+			: FindRow<Tail, [...Accumulator, any]>
+		: never
+
+type FindSanta<T extends any[]> = FindRow<MapRows<T>>
