3f34c6dd430c9440ed8908f5988818e2cccbc011
Sergey Ufocoder <xufocoder@gmail.com>
2023-12-17 16:01:26 +0300

    fix: solution of day-17

--- day-17/solution.ts
+++ day-17/solution.ts
@@ -10,5 +10,5 @@ type WhoWins<T extends RockPaperScissors, U extends RockPaperScissors>
   = T extends U
     ? 'draw'
     : WinnerMap[T] extends U
-      ? 'win' 
-      : 'lose'
+      ? 'lose' 
+      : 'win'
