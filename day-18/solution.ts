ee03acfe246d3a68f41e0ecab35bc14992ab11d7
Sergey Ufocoder <xufocoder@gmail.com>
2023-12-26 14:15:31 +0300

    chore: improve day-18 solution

--- day-18/solution.ts
+++ day-18/solution.ts
@@ -1,6 +1,6 @@
-export type Count<T extends any[], Letter extends any, Accumulator extends any[] = []> 
-  = T extends [infer Head, ...infer Tail]
+export type Count<T extends string[], Letter extends string, Accumulator extends string[] = []> 
+  = T extends [infer Head extends string, ...infer Tail extends string[]]
     ? Head extends Letter
-      ? Count<Tail, Letter, [...Accumulator, true]>
+      ? Count<Tail, Letter, [...Accumulator, Head]>
       : Count<Tail, Letter, Accumulator>
     : Accumulator['length']
