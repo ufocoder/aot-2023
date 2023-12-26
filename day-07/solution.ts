e176db06987686a3e0c422dbd7feff1d6252a628
Sergey Ufocoder <xufocoder@gmail.com>
2023-12-26 14:15:37 +0300

    chore: improve day-07 solution

--- day-07/solution.ts
+++ day-07/solution.ts
@@ -1,5 +1,5 @@
-export type AppendKey<T> = T extends string ? `good_${T}`: T;
+type GoodKey<T> = T extends string ? `good_${T}`: T;
 
 export type AppendGood<T> = {
-  [P in keyof T as AppendKey<P>]: T[P]
+  [P in keyof T as GoodKey<P>]: T[P]
 }
